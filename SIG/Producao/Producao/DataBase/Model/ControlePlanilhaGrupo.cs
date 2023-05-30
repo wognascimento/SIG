fb11ec4d5f6552218c2d1b7b6244f2b05615bacf
Wesley Oliveira <wog.nascimento@gmail.com>
2023-05-29 21:02:28 -0300

    CONTROLE POR GRUPO

--- SIG/Producao/Producao/DataBase/Model/ControlePlanilhaGrupo.cs
+++ SIG/Producao/Producao/DataBase/Model/ControlePlanilhaGrupo.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Producao
@@ -7,9 +8,9 @@ namespace Producao
     [Table("view_controle_planilha_grupo", Schema = "producao")]
     public class ControlePlanilhaGrupo
     {
-        public string? data_de_expedicao { get; set; }
+        public DateTime? data_de_expedicao { get; set; }
         public string? id { get; set; }
-        public string? fechamento_shopp { get; set; }
+        public DateTime? fechamento_shopp { get; set; }
         public string? revisado { get; set; }
         public string? confirmado { get; set; }
         public string? justificativa { get; set; }
@@ -21,26 +22,26 @@ namespace Producao
         public string? local_shoppings { get; set; }
         public string? descricao { get; set; }
         public string? descricao_adicional { get; set; }
-        public string? qtd_complemento { get; set; }
-        public string? qtd_detalhe { get; set; }
-        public string? qtd_exp { get; set; }
+        public double? qtd_complemento { get; set; }
+        public double? qtd_detalhe { get; set; }
+        public double? qtd_exp { get; set; }
         public string? complementoadicional { get; set; }
         public string? unidade { get; set; }
-        public string? producao { get; set; }
+        public DateTime? producao { get; set; }
         public string? supermercado { get; set; }
-        public string? enviado_baia { get; set; }
+        public DateTime? enviado_baia { get; set; }
         public string? obs_planilheiro { get; set; }
         public string? req { get; set; }
         public string? os { get; set; }
         public string? transf { get; set; }
         public string? orient_montagem { get; set; }
         public string? obs { get; set; }
-        public string? codcompl { get; set; }
-        public string? coddetalhescompl { get; set; }
+        public long? codcompl { get; set; }
+        public long? coddetalhescompl { get; set; }
         public string? item { get; set; }
         public string? resp_cenas { get; set; }
         public string? resp_trilha { get; set; }
-        public string? num_os_produto { get; set; }
+        public long? num_os_produto { get; set; }
         public string? caminhao { get; set; }
         public string? agrupamento { get; set; }
         public string? resp_estruturas { get; set; }
